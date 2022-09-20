bfc1b9351f69bfabdaf6ac5c6bc7f37b9d5c7e36
Nurmagomed Isakhaev <isakhaev2014@gmail.com>
2022-09-20 18:26:19 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+      <div className='app-wrapper'>
+        <header className='header'>
+          <img src='https://toppng.com/design-for-logo-PNG-free-PNG-Images_90491'/>
+        </header>
+        <nav className='nav'>
+          <div className='content'>
+            Profile
+          </div>
+          <div>
+            Messages
+          </div>
+        </nav>
+        <div>
+          Main content
+        </div>
+      </div>
+  )
+
+}
+
+export default App;
