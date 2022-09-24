e5f18b7e36a1cd56070a9710533328e95ce54018
Nurmagomed Isakhaev <isakhaev2014@gmail.com>
2022-09-24 15:18:17 +0300

    New logo, add new puds of menu

--- src/App.js
+++ src/App.js
@@ -5,18 +5,46 @@ const App = () => {
   return (
       <div className='app-wrapper'>
         <header className='header'>
-          <img src='https://toppng.com/design-for-logo-PNG-free-PNG-Images_90491'/>
+          <img src='https://chpic.su/_data/stickers/g/Gachimuchigang/Gachimuchigang_004.webp'/>
         </header>
         <nav className='nav'>
-          <div className='content'>
-            Profile
+          <div>
+            <a>Profile</a>
           </div>
           <div>
-            Messages
+            <a>Messages</a>
           </div>
+            <div>
+                <a>News</a>
+            </div>
+            <div>
+                <a>Music</a>
+            </div>
+            <div>
+                <a>Settings</a>
+            </div>
         </nav>
-        <div>
-          Main content
+        <div className='content'>
+            <div>
+                <img src="https://lh3.googleusercontent.com/VfZd9ZxqnEZmTNH6aJwgnoa54Jg-EyZsjUV7qxYS6AY0h9-pgdGki667y8zuHUe94ec=h500"/>
+            </div>
+            <div>
+                ava+descriptions
+            </div>
+            <div>
+                My posts
+                <div>
+                    New posts
+                </div>
+            </div>
+            <div>
+                <div>
+                    Post1
+                </div>
+                <div>
+                    Post2
+                </div>
+            </div>
         </div>
       </div>
   )
