c2055ea9764b02e9f540d347ccf03fd84bc55409
Nurmagomed Isakhaev <isakhaev2014@gmail.com>
2022-09-25 19:27:09 +0300

    New react components

--- src/App.js
+++ src/App.js
@@ -1,51 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import Content from "./components/Content";
 
 const App = () => {
   return (
       <div className='app-wrapper'>
-        <header className='header'>
-          <img src='https://chpic.su/_data/stickers/g/Gachimuchigang/Gachimuchigang_004.webp'/>
-        </header>
-        <nav className='nav'>
-          <div>
-            <a>Profile</a>
-          </div>
-          <div>
-            <a>Messages</a>
-          </div>
-            <div>
-                <a>News</a>
-            </div>
-            <div>
-                <a>Music</a>
-            </div>
-            <div>
-                <a>Settings</a>
-            </div>
-        </nav>
-        <div className='content'>
-            <div>
-                <img src="https://lh3.googleusercontent.com/VfZd9ZxqnEZmTNH6aJwgnoa54Jg-EyZsjUV7qxYS6AY0h9-pgdGki667y8zuHUe94ec=h500"/>
-            </div>
-            <div>
-                ava+descriptions
-            </div>
-            <div>
-                My posts
-                <div>
-                    New posts
-                </div>
-            </div>
-            <div>
-                <div>
-                    Post1
-                </div>
-                <div>
-                    Post2
-                </div>
-            </div>
-        </div>
+          <Header />
+          <NavBar />
+          <Content />
       </div>
   )
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = () => {
+    return <header className='header'>
+            <img src='https://chpic.su/_data/stickers/g/Gachimuchigang/Gachimuchigang_004.webp'/>
+        </header>
+}
+
+export default Header;
