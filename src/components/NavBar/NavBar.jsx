ec0a6483b2fb4e7966c4cf56142a98c4719a4c77
Nurmagomed Isakhaev <isakhaev2014@gmail.com>
2022-10-01 12:51:18 +0300

    Add Navbar, and new styles

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 import s from './NavBar.module.css';
+import {NavLink} from "react-router-dom";
 
 const NavBar = () => {
-    return <nav className={s.nav}>
+    return (
+        <nav className={s.nav}>
         <div className={s.item}>
-            <a href="/profile">Profile</a>
+            <NavLink to="/profile">Profile</NavLink>
         </div>
         <div className={s.item}>
-            <a href="/dialogs">Messages</a>
+            <NavLink to="/dialogs">Messages</NavLink>
         </div>
         <div className={s.item}>
-            <a>News</a>
+            <NavLink>
+                News
+            </NavLink>
         </div>
         <div className={s.item}>
-            <a>Music</a>
+            <NavLink>
+                Music
+            </NavLink>
         </div>
         <div className={s.item}>
-            <a>Settings</a>
+            <NavLink>
+                Settings
+            </NavLink>
         </div>
     </nav>
+    )
 }
 
 export default NavBar;
