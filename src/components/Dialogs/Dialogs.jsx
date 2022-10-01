aca5a2ffc66b9b71c954ee81090865b55ffe941b
Nurmagomed Isakhaev <isakhaev2014@gmail.com>
2022-10-01 13:40:35 +0300

    design of the Dialogs tab

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,9 +3,33 @@ import  s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    Van Darkholme
+                </div>
+                <div className={s.dialog}>
+                    Billy
+                </div>
+                <div className={s.dialog}>
+                    Boss of Gym
+                </div>
+                <div className={s.dialog}>
+                    Yuliya
+                </div>
+                <div className={s.dialog}>
+                    Nikis
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>My name is Van</div>
+                <div className={s.message}>Ouh shit, i'm sorry</div>
+                <div className={s.message}>Boy next door!</div>
         </div>
+    </div>
     )
 }
 
