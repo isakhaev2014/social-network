c237fc25c87497c0a9d0fe338b378f1a008c9b1c
Nurmagomed Isakhaev <isakhaev2014@gmail.com>
2022-10-09 04:53:20 +0300

    Packed data into State.js

--- src/App.js
+++ src/App.js
@@ -9,19 +9,21 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 const App = (props) => {
 
     return (
-      <BrowserRouter>
-      <div className='app-wrapper'>
-          <Header />
-          <NavBar />
-          <div className='app-wrapper-content'>
-              <Routes>
-                  <Route path="profile" element={<Profile posts={props.posts} />} />
-                  <Route path="dialogs" element={<Dialogs dialogs={props.dialogs} messages={props.messages}/>} />
-              </Routes>
-          </div>
-      </div>
-      </BrowserRouter>
-  )
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <NavBar/>
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path="profile"
+                               element={<Profile state={props.state.profilePage}/>}/>
+                        <Route path="dialogs"
+                               element={<Dialogs state={props.state.messagesPage}/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    )
 }
 
 export default App;
---
+++ src/redux/State.js
@@ -0,0 +1,30 @@
+let state = {
+
+    profilePage: {
+        posts: [
+            {id: 1, message: 'Ouh, shit, i sorry', likesCount: 6},
+            {id: 2, message: 'My name is Van', likesCount: 69}
+        ]
+    },
+
+    messagesPage: {
+        dialogs: [
+            {id: 1, name: 'Van Darkholme'},
+            {id: 2, name: 'Billy'},
+            {id: 3, name: 'Boss of Gym'},
+            {id: 4, name: 'Yuliya'},
+            {id: 5, name: 'Nikis'},
+            {id: 6, name: 'Andrey'}
+        ],
+        messages: [
+            {id: 1, message: 'My name is Van'},
+            {id: 2, message: 'Ouh shit, im sorry'},
+            {id: 3, message: 'Boy next door!'},
+            {id: 4, message: 'Где коммиты псина?'},
+            {id: 5, message: 'Ты не видел моего сынка?'},
+            {id: 6, message: 'Привет'}
+        ]
+    }
+}
+
+export default state;
