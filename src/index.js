7df27c2b183bcbc601d52186a1650deb2d4b8adc
Nurmagomed Isakhaev <isakhaev2014@gmail.com>
2022-10-27 20:46:54 +0300

    Added Redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import reportWebVitals from './reportWebVitals';
-import store from "./redux/State";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import store from './redux/redux-store'
 
 let renderEntireTree = (state) => {
     const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,7 +18,10 @@ let renderEntireTree = (state) => {
 
 renderEntireTree(store.getState())
 
-store.subscribe(renderEntireTree)
+store.subscribe(()=> {
+    let state = store.getState()
+    renderEntireTree(state)
+})
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,14 @@
 const ADD_POST = 'ADD-POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 
-const profileReducer = (state, action) => {
+let initialState = {
+    posts: [{id: 1, message: 'Ouh, shit, i sorry', likesCount: 6}, {
+        id: 2, message: 'My name is Van', likesCount: 69
+    }],
+    newPostText: 'Добавь пост, Leatherman'
+}
+
+const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST:
             let newPost = {
